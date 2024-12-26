01044aac8ad991fda495c095734dda23e4bb5f8b
MJ AHMAD <167817895+MJ-AHMAD@users.noreply.github.com>
2024-12-27 01:59:53 +0600

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -7,7 +7,7 @@ const person = {
         return `${this.firstName} ${this.lastName}`;
     },
     role:      'Microsoft Developer',
-    avatar:    'https://mj-ahmad.github.io/MJ-AHMAD-Insights/img/mja.png',
+    avatar:    '/img/mja.png',
     location:  'Asia/Dhaka',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English', 'Arabic']  // optional: Leave the array empty if you don't want to display languages
 }
